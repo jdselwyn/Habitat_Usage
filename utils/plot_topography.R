b2f8e9736aa93a520048f6f09a5ff03e13399eb5
jdselwyn <jdselwyn@gmail.com>
2021-07-26 16:06:01 -0500

    Update plot_topography.R

--- utils/plot_topography.R
+++ utils/plot_topography.R
@@ -17,18 +17,24 @@ site_rename <- tibble(Site = str_c('BZ17-', c('0A', '0B', '100N', '100S', '10KN'
                       site = c('F', 'G', 'E', 'I', 'A', 'C', 'K', 'D', 'J', 'B', 'L', 'H'))
 
 
+
 all_topography <- list.files(dir_to_look_in, pattern = 'stack.*tif$', full.names = TRUE) %>%
   tibble(file = .) %>%
   mutate(Site = str_extract(file, 'BZ17-[0-9ABNSK]+')) %>%
   rowwise %>%
   mutate(names = list(terra::rast(file) %>% names)) %>%
-  mutate(topography = list(read_stars(file, proxy = FALSE) %>%
+  mutate(topography = list(terra::rast(file) %>% 
+                             terra::trim() %>% 
+                             raster::stack() %>%
+                             st_as_stars)) %>%
+  mutate(topography = list(topography %>%
                              split('band') %>%
                              set_names(names) %>%
                              select(one_of(vars_to_keep)) %>%
                              mutate(depth = depth * -1,
-                                    moran = moran * -1)
-                           )) %>%
+                                    moran = moran * -1,
+                                    classified_habitat_unsmoothed = if_else(classified_habitat_unsmoothed == 1, 
+                                                                            'Sand', 'Reef')))) %>%
   select(-file, -names) %>%
   left_join(site_rename, by = 'Site') %>%
   arrange(site)
@@ -40,11 +46,11 @@ all_plots <- all_topography %>%
   rowwise %>%
   mutate(all_plots = list(map(plot_names, ~ggplot() +
                                 geom_stars(data = select(topography, one_of(.x))) +
-                                coord_equal() +
+                                coord_sf() +
                                 theme_void() +
-                                scale_fill_viridis(na.value = "white") +
                                 scale_x_discrete(expand = c(0, 0)) +
                                 scale_y_discrete(expand = c(0, 0)) +
+                                labs(fill = NULL) +
                                 theme(plot.background = element_blank(),
                                       panel.background = element_blank())
                               
@@ -56,17 +62,22 @@ all_plots <- all_topography %>%
                                 plot_names == 'boundaryDistance' ~ 'Distance to Sand/Reef Margin',
                                 plot_names == 'classified_habitat_unsmoothed' ~ 'Habitat Type',
                                 plot_names == 'vectorDispersion' ~ 'Vector Dispersion',
-                                TRUE ~ str_to_sentence(plot_names))) 
+                                TRUE ~ str_to_sentence(plot_names))) %>%
+  rowwise %>%
+  mutate(all_plots = list(all_plots + labs(title = plot_titles)),
+         all_plots = if_else(plot_names == 'classified_habitat_unsmoothed',
+                             list(all_plots + scale_fill_viridis(na.value = "white", discrete = TRUE, 
+                                                                 na.translate = FALSE)),
+                             list(all_plots + scale_fill_viridis(na.value = "white", discrete = FALSE))))
 
 
 
 wrapped_plots <- all_plots %>%
-  group_by(plot_names, plot_titles) %>%
-  summarise(plot = list(wrap_plots(all_plots) + plot_annotation(title = plot_titles, tag_levels = 'A')),
+  group_by(Site, site) %>%
+  summarise(plot = list(wrap_plots(all_plots) + plot_annotation(title = site)),
             .groups = 'drop') %>%
-  mutate(file_out = str_c('/work/hobi/jselwyn/Habitat/Results/Topography/', plot_names, '.png')) %>%
+  mutate(file_out = str_c('/work/hobi/jselwyn/Habitat/Results/Topography/', Site, '.png')) %>%
   ungroup %>%
   mutate(write_out = map2(plot, file_out, ~ggsave(filename = .y, plot = .x, height = 15, width = 15))) %>%
   select(-write_out)
-
 
